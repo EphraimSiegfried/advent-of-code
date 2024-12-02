bdf02c01a2722def896b32349a777b49841f76ce
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2024-12-02 09:47:04 +0100

    solve day02: part2

--- day02/part1.py
+++ day02/part1.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 
+def is_safe(levels):
+    diff = np.diff(levels)
+    return np.all((1 <= np.abs(diff)) & (np.abs(diff) <= 3)) and (
+        np.all(diff > 0) or np.all(diff < 0)
+    )
+
+
 if __name__ == "__main__":
 
     count = 0
@@ -8,8 +15,5 @@ if __name__ == "__main__":
     with open("input.txt") as input:
         for report in input:
             levels = np.fromstring(report, dtype=int, sep=" ")
-            diff = np.diff(levels)
-            count += np.all((1 <= np.abs(diff)) & (np.abs(diff) <= 3)) and (
-                np.all(diff > 0) or np.all(diff < 0)
-            )
+            count += is_safe(levels)
     print(count)
