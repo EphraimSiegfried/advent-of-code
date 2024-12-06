1bf7cfdb7867904b22be5f57958143cbf98ce580
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2024-12-06 15:10:16 +0100

    day06: fix part2

--- day06/part2.py
+++ day06/part2.py
@@ -2,28 +2,29 @@ from part1 import find_start, parse_input, move, pretty_print
 import copy
 
 
-def is_loopy(map):
-    map = copy.deepcopy(map)
-    num_cols = len(map[0])
-    num_rows = len(map)
-    x, y = find_start(map)
+def is_loopy(mapp, x, y):
+    mapp = copy.deepcopy(map)
+    mapp[x][y] = "#"
+    num_cols = len(mapp[0])
+    num_rows = len(mapp)
+    x, y = find_start(mapp)
     direction = 0
     in_bounds = lambda x, y: 0 <= x < num_cols and 0 <= y < num_rows
+    visited = set()
 
     # simulate
     while True:
-        # pretty_print(map)
-        map[y][x] = str(direction)
         x_prev, y_prev = x, y
         x, y = move(direction, x, y)
         if not in_bounds(x, y):
             break
-        if map[y][x] == "#":
+        if mapp[y][x] == "#":
+            node = (x,y,direction)
+            if node in visited:
+                return True
+            visited.add((x,y,direction))
             x, y = x_prev, y_prev
             direction = (direction + 1) % 4
-        elif map[y][x] == str(direction):
-            return True
-        map[y][x] = "^"
     return False
 
 
@@ -31,12 +32,13 @@ if __name__ == "__main__":
 
     map = parse_input("input.txt")
     count = 0
+
     # brute force hehe :)
+    # https://preview.redd.it/swmwroeby8171.png?auto=webp&s=7c18bca81baefa11330a821ce78305445ac8556d
+    i = 0
     for y in range(len(map)):
         for x in range(len(map[0])):
             if map[x][y] == ".":
-                new_map = copy.deepcopy(map)
-                new_map[x][y] = "#"
-                if is_loopy(new_map):
+                if is_loopy(map,x,y):
                     count += 1
     print(count)
