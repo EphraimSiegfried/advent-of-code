123b6c0dd0231966da644621cc5157dfe4a71b26
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-08-12 21:29:41 +0200

    solve day 13

--- 2023/src/day13.rs
+++ 2023/src/day13.rs
@@ -1,20 +1,128 @@
 use crate::util::grid::Grid;
 
 pub fn part1(input: &str) -> String {
-    let grids = input.split('\n').map(|p| Grid::parse(p));
-    grids.map(|g| g);
-    input.to_string()
+    let grids = input.split("\n\n").map(|p| Grid::parse(p));
+    grids
+        .map(|grid| {
+            if let Some(num_cols) = solve_cols(&grid, None) {
+                return 1 + num_cols;
+            }
+            if let Some(num_rows) = solve_rows(&grid, None) {
+                return 100 * (1 + num_rows);
+            }
+            grid.print();
+            panic!("No reflection found");
+        })
+        .sum::<usize>()
+        .to_string()
 }
 pub fn part2(input: &str) -> String {
-    input.to_string()
+    let grids = input.split("\n\n").map(|p| Grid::parse(p));
+    grids
+        .map(|grid| {
+            let candidates = candidate_grids(&grid);
+            let num_cols_org = solve_cols(&grid, None);
+            let num_rows_org = solve_rows(&grid, None);
+            grid.print();
+            for candidate in &candidates {
+                print!("\n");
+                candidate.print();
+                if let Some(num_cols_new) = solve_cols(&candidate, num_cols_org) {
+                    return dbg!(num_cols_new) + 1;
+                }
+                if let Some(num_rows_new) = solve_rows(&candidate, num_rows_org) {
+                    return 100 * (dbg!(num_rows_new) + 1);
+                }
+            }
+            panic!("no reflection found")
+        })
+        .sum::<usize>()
+        .to_string()
 }
 
-fn identical_pairs(vecs: &Vec<&Vec<char>>) -> Vec<(usize, usize)> {
+fn solve_cols(grid: &Grid, exclude: Option<usize>) -> Option<usize> {
+    find_reflection(
+        &identical_pairs(grid.columns()),
+        grid.num_columns(),
+        exclude,
+    )
+}
+
+fn solve_rows(grid: &Grid, exclude: Option<usize>) -> Option<usize> {
+    find_reflection(&identical_pairs(grid.rows()), grid.num_rows(), exclude)
+}
+
+fn candidate_grids(grid: &Grid) -> Vec<Grid> {
+    // ugly as fuck but whatever
+    let mut grids = Vec::new();
+    for (i, r1) in grid.rows().enumerate() {
+        let row1_string = r1.collect::<String>();
+        for (j, r2) in grid.rows().enumerate() {
+            if i > j || (j - i) % 2 == 0 {
+                continue;
+            }
+            let row2_string = r2.collect::<String>();
+            if let Some(idx) = differ_by_one(row1_string.as_str(), row2_string.as_str()) {
+                for row in [i, j] {
+                    let mut new_grid = grid.clone();
+                    let symbol = if grid.at(row, idx).unwrap() == '.' {
+                        '#'
+                    } else {
+                        '.'
+                    };
+                    new_grid.set(row, idx, symbol);
+                    grids.push(new_grid);
+                }
+            }
+        }
+    }
+    for (i, r1) in grid.columns().enumerate() {
+        let col1_string = r1.collect::<String>();
+        for (j, r2) in grid.columns().enumerate() {
+            if i > j || (j - i) % 2 == 0 {
+                continue;
+            }
+            let col2_string = r2.collect::<String>();
+            if let Some(idx) = differ_by_one(col1_string.as_str(), col2_string.as_str()) {
+                for col in [i, j] {
+                    let mut new_grid = grid.clone();
+                    let symbol = if grid.at(idx, col).unwrap() == '.' {
+                        '#'
+                    } else {
+                        '.'
+                    };
+                    new_grid.set(idx, col, symbol);
+                    grids.push(new_grid);
+                }
+            }
+        }
+    }
+    grids
+}
+
+fn differ_by_one(a: &str, b: &str) -> Option<usize> {
+    let mut diff_index = None;
+    for (i, (c1, c2)) in a.chars().zip(b.chars()).enumerate() {
+        if c1 != c2 {
+            if diff_index.is_some() {
+                return None;
+            }
+            diff_index = Some(i);
+        }
+    }
+
+    diff_index
+}
+
+fn identical_pairs<'a, Outer, Inner>(vecs: Outer) -> Vec<(usize, usize)>
+where
+    Outer: Iterator<Item = Inner>,
+    Inner: IntoIterator<Item = &'a char>,
+{
     let mut pairs: Vec<(usize, usize)> = Vec::new();
     let mut enum_vecs: Vec<(usize, Vec<char>)> = vecs
-        .iter()
         .enumerate()
-        .map(|(i, r)| (i, (*r).clone()))
+        .map(|(i, r)| (i, r.into_iter().copied().collect::<Vec<char>>()))
         .collect();
 
     while let Some((j, y)) = enum_vecs.pop() {
@@ -27,11 +135,25 @@ fn identical_pairs(vecs: &Vec<&Vec<char>>) -> Vec<(usize, usize)> {
     pairs
 }
 
-fn find_reflection(pairs: &Vec<(usize, usize)>) -> usize {
-    let (mut x, mut &y) = pairs.iter().find(|&(x, y)| x.abs_diff(*y) == 1).unwrap();
-    while pairs.contains(&(x, y)) {
-        x -= 1;
-        y += 1;
+fn find_reflection(
+    pairs: &[(usize, usize)],
+    limit: usize,
+    exclude: Option<usize>,
+) -> Option<usize> {
+    let candidates = pairs.iter().filter(|(x, y)| x.abs_diff(*y) == 1);
+    for candidate in candidates {
+        let (x_start, y_start) = candidate;
+        let mut x_current = *x_start;
+        let mut y_current = *y_start;
+        while x_current > 0 && pairs.contains(&(x_current - 1, y_current + 1)) {
+            x_current -= 1;
+            y_current += 1;
+        }
+        if (x_current == 0 || y_current == limit - 1)
+            && (exclude.is_none() || exclude.unwrap() != *x_start)
+        {
+            return Some(*x_start);
+        }
     }
-    x
+    None
 }
--- 2023/src/util/grid.rs
+++ 2023/src/util/grid.rs
@@ -1,5 +1,6 @@
 use crate::util::point::Point;
 
+#[derive(Debug, Clone)]
 pub struct Grid {
     mat: Vec<Vec<char>>,
 }
@@ -10,12 +11,15 @@ impl Grid {
         Grid { mat: mat }
     }
 
-    pub fn at(&self, point: Point) -> Option<char> {
-        let Point(row_idx, col_idx) = point;
-        if !self.in_bounds(point) {
+    pub fn at(&self, row_idx: usize, col_idx: usize) -> Option<char> {
+        if !self.in_bounds(row_idx, col_idx) {
             return None;
         }
         Some(self.mat[row_idx as usize][col_idx as usize])
+    }
+
+    pub fn set(&mut self, row_idx: usize, col_idx: usize, c: char) {
+        self.mat[row_idx][col_idx] = c;
     }
 
     pub fn find(&self, target: char) -> Vec<Point> {
@@ -30,11 +34,10 @@ impl Grid {
         result
     }
 
-    pub fn in_bounds(&self, point: Point) -> bool {
-        let Point(row_idx, col_idx) = point;
+    pub fn in_bounds(&self, row_idx: usize, col_idx: usize) -> bool {
         let num_rows = self.mat.len();
         let num_cols = self.mat[0].len();
-        0 <= row_idx && row_idx < num_rows as i32 && 0 <= col_idx && col_idx < num_cols as i32
+        row_idx < num_rows && col_idx < num_cols
     }
 
     pub fn print(&self) {
@@ -45,26 +48,20 @@ impl Grid {
             print!("\n");
         }
     }
-    pub fn columns(&self) -> impl Iterator<Item = Vec<char>> + '_ {
-        if self.mat.is_empty() || self.mat[0].is_empty() {
-            // Return an empty iterator for an empty or malformed grid
-            return Vec::new().into_iter();
-        }
-
-        let num_rows = self.mat.len();
-        let num_cols = self.mat[0].len();
-
-        (0..num_cols)
-            .map(move |col_idx| {
-                (0..num_rows)
-                    .map(|row_idx| self.mat[row_idx][col_idx])
-                    .collect::<Vec<char>>() // Collect inner iterator into a Vec<char>
-            })
-            .collect::<Vec<Vec<char>>>() // Collect the outer iterator into a Vec<Vec<char>>
-            .into_iter() // Convert the Vec<Vec<char>> into an iterator
+    pub fn num_columns(&self) -> usize {
+        self.mat[0].len()
     }
 
-    pub fn rows(&self) -> impl Iterator<Item = &Vec<char>> + '_ {
-        self.mat.iter()
+    pub fn num_rows(&self) -> usize {
+        self.mat.len()
+    }
+
+    pub fn columns(&self) -> impl Iterator<Item = impl Iterator<Item = &char>> {
+        let num_cols = self.mat[0].len();
+        (0..num_cols).map(move |col_idx| self.mat.iter().map(move |row| &row[col_idx]))
+    }
+
+    pub fn rows(&self) -> impl Iterator<Item = impl Iterator<Item = &char>> {
+        self.mat.iter().map(|row| row.iter())
     }
 }
